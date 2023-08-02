c12e77dba17ffc60c1782c1d1e94542b8b94b409
phasewalk <100740825+phasewalk1@users.noreply.github.com>
2023-08-02 15:22:58 -0700

    Delete contracts/build directory

--- contracts/build/trucks.contract.lua
+++
@@ -1,8 +0,0 @@
-local trucks = require("trucks")
-trucks["mkconfig!"]()
-trucks["system!"]({panic = "unwind", verbose = "true"})
-trucks["defaults!"]({["run-ctx"] = "npm", cmd = "run devstart", sync = "npm install"})
-trucks["service!"]("fe", {github = "mattg1243/sb-frontend"})
-trucks["service!"]("courier", {github = "phasewalk1/courier", ["run-ctx"] = "cargo", cmd = "watch-x run"})
-local contract = trucks["contract!"]()
-return contract
